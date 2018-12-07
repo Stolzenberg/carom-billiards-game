252c74c34abd50f3c64f90da38691f8ad7431775
Tim Stolzenberg <timstolzenberg@outlook.de>
2018-12-07 20:27:26 +0100

    + Added user interface canvas + Added user interface controller # Edited the float reference script # Moved pressed time property variable from player controller to own float reference file

--- Unity/Assets/Scripts/Variables/Reference/FloatReference.cs
+++ Unity/Assets/Scripts/Variables/Reference/FloatReference.cs
@@ -21,6 +21,18 @@ namespace CaromBilliardsGame.Stolzenberg.Variables
         public float Value
         {
             get { return UseConstant ? ConstantValue : Variable.Value; }
+
+            set
+            {
+                if (UseConstant)
+                {
+                    ConstantValue = value;
+                }
+                else
+                {
+                    Variable.Value = value;
+                }
+            }
         }
 
         public static implicit operator float(FloatReference reference)
