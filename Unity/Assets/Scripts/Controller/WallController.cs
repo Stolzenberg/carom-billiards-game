24913a57446bee6fabbfc26b1704a0ebcf17798e
Tim Stolzenberg <tim.stolzenberg@selfbeat.com>
2018-12-07 16:06:36 +0100

    # Worked on the wall controller and model

---
+++ Unity/Assets/Scripts/Controller/WallController.cs
@@ -0,0 +1,14 @@
+﻿using CaromBilliardsGame.Stolzenberg.Models;
+using UnityEngine;
+
+namespace CaromBilliardsGame.Stolzenberg.Controllers
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class WallController : MonoBehaviour
+    {
+        public WallModel WallModel { get { return wallModel; } }
+
+        [Header("Model")]
+        [SerializeField] private WallModel wallModel;
+    }
+}
---
+++ Unity/Assets/Scripts/Models/WallModel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace CaromBilliardsGame.Stolzenberg.Models
+{
+    [CreateAssetMenu]
+    public class WallModel : ScriptableObject
+    {
+        public MirrorAxisEnum MirrorAxis;
+        public enum MirrorAxisEnum { X, Y }
+    }
+}
