10419343f795177df5b7b9ef1630db4ebb2cbb1d
Tim Stolzenberg <tim.stolzenberg@selfbeat.com>
2018-12-17 20:26:14 +0100

    + Added quit game button to menu

---
+++ Unity/Assets/Scripts/Controller/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace CaromBilliardsGame.Stolzenberg.Controllers
+{
+    public class MenuController : MonoBehaviour
+    {
+        public void CloseApplication()
+        {
+            Application.Quit();
+        }
+    }
+}
